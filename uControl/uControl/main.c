c186589b11a510b5141022257ef28cdbe46afa51
Mason Anderson <mason.csgotrading@gmail.com>
2022-03-19 15:01:51 -0400

    added microchip project

---
+++ uControl/uControl/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/19/2022 3:01:25 PM
+ *  Author: mason
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
