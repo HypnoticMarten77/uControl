c117513c389554942228e2276f557d1541dc201a
Andres Maldonado-Martin <andresdmm1@hotmail.com>
2022-04-06 03:01:42 -0400

    Update manifold to include correct BT permissions for app

---
+++ app/src/main/java/com/example/ucontrolcompanion/BluetoothTest.java
@@ -0,0 +1,14 @@
+package com.example.ucontrolcompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BluetoothTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_test);
+    }
+}
